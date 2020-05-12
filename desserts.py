124c16b6af9bf9e5af654470e3b7f5da77c0f72f
Shira Amrany <shira.amrany@gmail.com>
2020-05-12 00:43:18 +0000

    all less class/static methods

--- desserts.py
+++ desserts.py
@@ -4,10 +4,53 @@
 class Cupcake:
     """A cupcake."""
 
+    cache = {}
+
+    def __init__(self, name, flavor, price):
+    	self.name = name
+    	self.flavor = flavor
+    	self.price = price
+    	self.qty = 0
+    	self.cache(self.name)
+
+
     def __repr__(self):
         """Human-readable printout for debugging."""
 
         return f'<Cupcake name="{self.name}" qty={self.qty}>'
+
+
+    # @classmethod
+    # def cache(cls, name):
+    # 	"""store all cupcake instances by name"""
+
+ 
+    	
+    def get_cupcake(cls, name):
+    	cls.cache(name)
+
+    def add_stock(self, amount):	
+     	self.qty += amount
+
+    def sell(self, amount):
+    	if amount < self.qty:
+    		self.qty = self.qty - amount
+    	elif self.qty == 0:
+    		print("Sorry, these cupcakes are sold out")
+    	else:
+    		print("there are only {} of these left".format(self.qty))
+    		self.qty = 0
+
+
+    # @staticmethod
+    # def scale_recipe(ingredients, amount):
+    # 	for ingredient in ingredients:
+    # 		[(ingredient, qty * amount)]
+
+    # 	return ingredients	
+
+
+
 
 
 if __name__ == '__main__':
